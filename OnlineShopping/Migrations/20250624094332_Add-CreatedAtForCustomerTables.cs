5b69423f800d74f1de2cd159eed11e334440b26c
James Kinyua <james.kinyua@agilebiz.co.ke>
2025-06-24 12:43:50 +0300

    Feat: Track when the customer was created

---
+++ OnlineShopping/Migrations/20250624094332_Add-CreatedAtForCustomerTables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShopping.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtForCustomerTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Customers");
+        }
+    }
+}
